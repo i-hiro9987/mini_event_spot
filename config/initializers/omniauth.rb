d0fe04c4ce9f5b126a5eef67b1914317f717169f
iwao <h_iwao_yri@uluru.jp>
2026-06-24 11:32:59 +0900

    github-auth settings

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,6 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :github,
+    Rails.application.credentials.dig(:github, :client_id),
+    Rails.application.credentials.dig(:github, :client_secret),
+    scope: "user:email"
+end
